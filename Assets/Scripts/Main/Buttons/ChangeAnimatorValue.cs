20357bf0dcb4ec9c3a132b20d6d33ec662e88600
Inferno-EK <54182739+Inferno-EK@users.noreply.github.com>
2020-04-19 20:22:03 +0300

    Creating main buttons and animation to buttons

---
+++ Assets/Scripts/Main/Buttons/ChangeAnimatorValue.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeAnimatorValue : MonoBehaviour
+{
+    [SerializeField] private Animator buttonAnimator;
+ 
+    public void TurningButtons(bool value)
+    {
+        buttonAnimator.SetBool("IsON", value);
+    }
+    public void TurningButtons()
+    {
+        buttonAnimator.SetBool("IsON", !buttonAnimator.GetBool("IsON"));
+    }
+
+}
---
+++ Assets/Scripts/Main/CameraSize.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+public class CameraSize : MonoBehaviour
+{
+    Camera _thisCamera;
+    Transform _transform;
+    void Start()
+    {
+        _thisCamera = GetComponent<Camera>();
+        _transform = transform;
+    }
+
+    void Update()
+    {
+        _thisCamera.orthographicSize = 0.5f * Screen.height;
+        _transform.position = new Vector3(Screen.width / 2f, Screen.height / 2f);
+    }
+}
